65ec90c6d9cb0cbdd7bb03ad7fb665ce28a92edb
Jaskirat Singh 12212295 <112509465+jaskirat-13@users.noreply.github.com>
2024-06-03 15:53:08 +0530

    First and last occurence of a key in array

---
+++ first_last_occurence.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+    int ans = -1;
+    while(start <= end){
+        if(key == arr[mid]){
+            ans = mid;
+            end = mid - 1;
+        }
+        else if(key > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+    int ans = -1;
+    while(start <= end){
+        if(key == arr[mid]){
+            ans = mid;
+            start = mid + 1;
+        }
+        else if(key > arr[mid]){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return ans;
+}
+
+int main(){
+    int arr[] = {1, 2, 3, 3, 3, 3, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int key;
+    cout<<"Enter key : ";
+    cin>> key;
+    
+    int first = firstOcc(arr, n, key);
+    cout<< "First occurence of element is at " << first << endl;
+    
+    int last = lastOcc(arr, n, key);
+    cout<< "Last occurence of element is at " << last << endl;
+    return 0;
+}
